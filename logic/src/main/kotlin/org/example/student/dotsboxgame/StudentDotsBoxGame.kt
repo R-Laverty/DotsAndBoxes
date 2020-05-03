ae2813432047b5d14196147e041fdb6e5f166908
Robert Laverty <robert.laverty1608@gmail.com>
2020-05-03 23:48:21 +0100

    Completed more of the logic

--- logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
@@ -5,15 +5,10 @@ import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.Matrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.MutableMatrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.MutableSparseMatrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.SparseMatrix
-import kotlin.random.Random
-
 
 class StudentDotsBoxGame(columns: Int = 8, rows: Int = 8, players: List<Player> =
     listOf(HumanPlayer(), HumanPlayer())) : AbstractDotsAndBoxesGame() {
     override val players: List<Player> = players
-    val mColumns: Int = columns
-    val mRows:Int = rows
-    var turn:Int = 0
 
     override var currentPlayer: Player = players[0]
         get()= players[0]
@@ -24,7 +19,7 @@ class StudentDotsBoxGame(columns: Int = 8, rows: Int = 8, players: List<Player> 
     override val lines: SparseMatrix<DotsAndBoxesGame.Line> = MutableSparseMatrix(rows, columns, ::StudentLine)//TODO("Create a matrix initialized with your own line type")
 
     override val isFinished: Boolean
-        get() = isFinished//TODO("Provide this getter. Note you can make it a var to do so")
+        get() = TODO("Provide this getter. Note you can make it a var to do so")
 
     override fun playComputerTurns() {
         var current = currentPlayer
@@ -40,46 +35,46 @@ class StudentDotsBoxGame(columns: Int = 8, rows: Int = 8, players: List<Player> 
      * it being an inner class.
      */
     inner class StudentLine(lineX: Int, lineY: Int) : AbstractLine(lineX, lineY) {
-        override var isDrawn: Boolean = false
-            get() = this.isDrawn//TODO("Provide this getter. Note you can make it a var to do so")
+        override val isDrawn: Boolean
+            get() = false//TODO("Provide this getter. Note you can make it a var to do so")
 
 
         override val adjacentBoxes: Pair<StudentBox?, StudentBox?>
-            get() {
-                //TODO("You need to look up the correct boxes for this to work")
-                var boxAboveOrLeft: StudentBox?
-                var boxBelowOrRight: StudentBox?
-                if (this.lineY == 0) {
-                    boxAboveOrLeft = null
+            get()  {
+                if (lineY == 0){
+                    return Pair(null, StudentBox(lineX,lineY))
+                }else if (lineX == 0) {
+                    return Pair(null, StudentBox(lineX,(lineY - 1) / 2))
+                }else{
+                    if (lineY % 2 == 0) {
+                        return Pair(
+                            StudentBox(lineX,(lineY - 2) / 2),
+                            StudentBox(lineX,lineY / 2)
+                                   )
+                    } else {
+                        return Pair(
+                            StudentBox(lineX - 1,(lineY - 1) / 2),
+                            StudentBox(lineX,(lineY - 1) / 2)
+                                   )
+                    }
                 }
-                if (this.lineY == mRows*2){
-                    boxBelowOrRight = null
-                }
-            }
+            }//TODO("You need to look up the correct boxes for this to work")
 
         override fun drawLine() {
             //TODO("Implement the logic for a player drawing a line. Don't forget to inform the listeners (fireGameChange, fireGameOver)")
             // NOTE read the documentation in the interface, you must also update the current player.
-            this.isDrawn = true
-            fireGameChange()
-            if (turn%2 == 0){
-                currentPlayer = players[0]
-            } else {
-                currentPlayer = players[1]
-            }
         }
     }
 
     inner class StudentBox(boxX: Int, boxY: Int) : AbstractBox(boxX, boxY) {
 
         override val owningPlayer: Player?
-            get() = TODO("Provide this getter. Note you can make it a var to do so")
+            get() = null//TODO("Provide this getter. Note you can make it a var to do so")
 
         /**
          * This must be lazy or a getter, otherwise there is a chicken/egg problem with the boxes
          */
         override val boundingLines: Iterable<DotsAndBoxesGame.Line>
-            get() = TODO("Look up the correct lines from the game outer class")
-
+            get() = lines.filter { it.adjacentBoxes.first == this || it.adjacentBoxes.second == this  }//TODO("Look up the correct lines from the game outer class")
     }
 }
