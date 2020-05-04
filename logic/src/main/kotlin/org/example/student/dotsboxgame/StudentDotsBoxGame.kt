1da760ffdc5653b32641626107609a715d2e7115
Robert Laverty <robert.laverty1608@gmail.com>
2020-05-04 15:57:12 +0100

    completed more of the logic as well as completing some testing

--- logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
+++ logic/src/main/kotlin/org/example/student/dotsboxgame/StudentDotsBoxGame.kt
@@ -5,19 +5,20 @@ import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.Matrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.MutableMatrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.MutableSparseMatrix
 import uk.ac.bournemouth.ap.dotsandboxeslib.matrix.SparseMatrix
+import java.lang.Exception
 
 class StudentDotsBoxGame(columns: Int = 8, rows: Int = 8, players: List<Player> =
     listOf(HumanPlayer(), HumanPlayer())) : AbstractDotsAndBoxesGame() {
-    override val players: List<Player> = players
+    override val players: List<Player> = players.toList()
 
     override var currentPlayer: Player = players[0]
-        get()= players[0]
+        //get()= players[0]
 
     // NOTE: you may want to me more specific in the box type if you use that type in your class
-    override val boxes: Matrix<DotsAndBoxesGame.Box> = MutableMatrix(columns, rows, ::StudentBox)//TODO("Create a matrix initialized with your own box type")
+    override val boxes: Matrix<StudentBox> = MutableMatrix(columns, rows, ::StudentBox)//TODO("Create a matrix initialized with your own box type")
 
     override val lines: SparseMatrix<DotsAndBoxesGame.Line> =
-        MutableSparseMatrix(columns+1, rows*2, ::StudentLine,
+        MutableSparseMatrix(columns+1, (rows*2)+1, ::StudentLine,
                             {x,y -> !(y%2==0 && x==columns)})//TODO("Create a matrix initialized with your own line type")
 
     override val isFinished: Boolean
@@ -37,45 +38,70 @@ class StudentDotsBoxGame(columns: Int = 8, rows: Int = 8, players: List<Player> 
      * it being an inner class.
      */
     inner class StudentLine(lineX: Int, lineY: Int) : AbstractLine(lineX, lineY) {
-        override val isDrawn: Boolean
-            get() = false//TODO("Provide this getter. Note you can make it a var to do so")
+        override var isDrawn: Boolean = false
+            //get() = false//TODO("Provide this getter. Note you can make it a var to do so")
 
 
         override val adjacentBoxes: Pair<StudentBox?, StudentBox?>
             get()  {
-                var boxLeftOrUp: StudentBox?
-                var boxRightOrDown: StudentBox?
-
-                if (lineY%2 == 0){
-                    boxLeftOrUp = StudentBox(lineX, (lineY-2)/2)
-                    boxRightOrDown = StudentBox(lineX, lineY/2)
+                if (lineY%2==0){
+                    if (lineY == 0){
+                        return Pair(null,boxes[lineX, lineY/2])
+                    }
+                    if (lineY == lines.maxHeight-1) {
+                        return Pair(boxes[lineX,(lineY-2)/2], null)
+                    }
+                    return Pair(boxes[lineX, (lineY-2)/2],boxes[lineX, lineY/2])
                 }else{
-                    boxLeftOrUp = StudentBox(lineX-1, (lineY-1)/2)
-                    boxRightOrDown = StudentBox(lineX, (lineY-1)/2)
+                    if (lineX == 0){
+                        return Pair(null, boxes[lineX,(lineY-1)/2])
+                    }
+                    if(lineX == lines.maxWidth-1){
+                        return Pair(boxes[lineX-1,(lineY-1)/2], null)
+                    }
+                    return Pair(boxes[lineX-1, (lineY-1)/2],boxes[lineX, (lineY-1)/2])
                 }
-                if (lineY == 0){
-                    boxLeftOrUp = null
-                }else if (lineX == 0 && lineY%2 != 0) {
-                    boxLeftOrUp = null
-                }
-                if (lineY == lines.maxHeight-1){
-                    boxRightOrDown = null
-                }else if (lineX == lines.maxWidth-1){
-                    boxRightOrDown = null
-                }
-                return Pair(boxRightOrDown,boxLeftOrUp)
             }//TODO("You need to look up the correct boxes for this to work")
 
         override fun drawLine() {
             //TODO("Implement the logic for a player drawing a line. Don't forget to inform the listeners (fireGameChange, fireGameOver)")
             // NOTE read the documentation in the interface, you must also update the current player.
+            if(this.isDrawn){
+                throw Exception("Line already drawn")
+            }
+            this.isDrawn = true
+            var changePlayer = true
+            //set boxes which do not have an owning player and have been drawn to belong to the current player
+            for (box in adjacentBoxes.toList()){
+                if (box != null) {
+                    if (box.boundingLines.filter { it.isDrawn }.count() == 4){
+                        box.owningPlayer = currentPlayer
+                        changePlayer = false
+                    }
+                }
+            }
+            if (boxes.filter { it.owningPlayer == null }.count() == 0){
+                val scores = getScores().mapIndexed{index, score -> Pair(players[index], score)}
+                fireGameOver(scores)
+            }
+            if (changePlayer) {
+                if (currentPlayer == players[0]) {
+                    currentPlayer = players[1]
+                } else {
+                    currentPlayer = players[0]
+                }
+            }
+            if (currentPlayer is ComputerPlayer){
+                playComputerTurns()
+            }
+            fireGameChange()
         }
     }
 
     inner class StudentBox(boxX: Int, boxY: Int) : AbstractBox(boxX, boxY) {
 
-        override val owningPlayer: Player?
-            get() = null//TODO("Provide this getter. Note you can make it a var to do so")
+        override var owningPlayer: Player? = null
+            //get() = null//TODO("Provide this getter. Note you can make it a var to do so")
 
         /**
          * This must be lazy or a getter, otherwise there is a chicken/egg problem with the boxes
